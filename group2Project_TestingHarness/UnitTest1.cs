fbe065a7f61059c6f85853fbab2d22583a194640
Chad Schadewald <ces83@students.uwf.edu>
2020-03-18 12:35:44 -0600

    Initial Testing Harness settup.

---
+++ group2Project_TestingHarness/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace group2Project_TestingHarness
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
