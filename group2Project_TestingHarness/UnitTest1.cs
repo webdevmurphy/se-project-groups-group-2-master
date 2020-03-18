6389de71881e2a03291da95ab6b54620b6221129
Chad Schadewald <ces83@students.uwf.edu>
2020-03-18 17:24:11 -0600

    Adding basic test method structure to the test harness.

--- group2Project_TestingHarness/UnitTest1.cs
+++ group2Project_TestingHarness/UnitTest1.cs
@@ -3,11 +3,165 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace group2Project_TestingHarness
 {
     [TestClass]
-    public class UnitTest1
+    public class CosmosConnection_Test
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CosmosConnection_getPassword()
         {
+           
+        }
+    }
+    [TestClass]
+    public class Robot_Test
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+
+        }
+    }
+    [TestClass]
+    public class Course_Test
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+
+        }
+    }
+    [TestClass]
+    public class Family_Test
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+
+        }
+    }
+    [TestClass]
+    public class Question_Test
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+
+        }
+    }
+    [TestClass]
+    public class triviaPlayer_Test
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+
+        }
+    }
+    [TestClass]
+    public class about_Test
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+
+        }
+    }
+    [TestClass]
+    public class CourseGrid_Test
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+
+        }
+    }
+    [TestClass]
+    public class CreateCourse_Test
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+
+        }
+    }
+    [TestClass]
+    public class CreateNewUser_Test
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+
+        }
+    }
+    [TestClass]
+    public class Login_Test
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+
+        }
+    }
+    [TestClass]
+    public class Mainmenu_test
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+
+        }
+    }
+    [TestClass]
+    public class mapView_Test
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+
+        }
+    }
+    [TestClass]
+    public class NewGame_Test
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+
+        }
+    }
+    [TestClass]
+    public class questionGrid_Test
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+
+        }
+    }
+    [TestClass]
+    public class Questions_Test
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+
+        }
+    }
+    [TestClass]
+    public class UpdateQuestion_Test
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+
+        }
+    }
+    [TestClass]
+    public class PopupQuestion_Test
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+
         }
     }
 }
