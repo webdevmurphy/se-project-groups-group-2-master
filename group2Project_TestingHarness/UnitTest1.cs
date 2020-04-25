f84fbd9a49c3ad5a616fe51e0fc89ab2cbcfdcd6
Chad Schadewald <ces83@students.uwf.edu>
2020-04-24 19:23:30 -0500

    Cleaning up some tests.

--- group2Project_TestingHarness/UnitTest1.cs
+++ group2Project_TestingHarness/UnitTest1.cs
@@ -6,7 +6,6 @@ using group2Project.Views;
 
 namespace group2Project_TestingHarness
 {
-
     [TestClass]
     public class Main_Test
     {
@@ -18,17 +17,35 @@ namespace group2Project_TestingHarness
 
         }
     }
-
     [TestClass]
     public class Student_Test
     {
         [TestMethod]
         public void TestMethod()
         {
-
+            string test_email = "test@test.com";
+            string test_first_name = "first";
+            string test_last_name = "last";
+            string test_username = "user";
+            string test_password = "pass";
+            int test_teacher_code = 2;
+            Student testStudent = new Student
+            {
+                Email = "test@test.com",
+                FirstName = "first",
+                LastName = "last",
+                UserName = "user",
+                PassWord = "pass",
+                TeacherCode = 2
+            };
+            Assert.AreEqual(test_email, testStudent.Email);
+            Assert.AreEqual(test_first_name, testStudent.FirstName);
+            Assert.AreEqual(test_last_name, testStudent.LastName);
+            Assert.AreEqual(test_username, testStudent.UserName);
+            Assert.AreEqual(test_password, testStudent.PassWord);
+            Assert.AreEqual(test_teacher_code, testStudent.TeacherCode);
         }
     }
-
     [TestClass]
     public class Trivia_Test
     {
@@ -57,7 +74,20 @@ namespace group2Project_TestingHarness
             Assert.AreEqual(question_4, testTrivia.Answer4);
         }
     }
-
+    [TestClass]
+    public class Courses_Test 
+    {
+        [TestMethod]
+        public void TestMethod() 
+        {
+            string course_name_test = "test";
+            Courses testCourses = new Courses
+            {
+                CourseName = "test"
+            };
+            Assert.AreEqual(course_name_test, testCourses.CourseName);
+        }
+    }
     [TestClass]
     public class LoginChecker_Test
     {
@@ -67,7 +97,6 @@ namespace group2Project_TestingHarness
 
         }
     }
-
     [TestClass]
     public class QuizBuilder_Test
     {
